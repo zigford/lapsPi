bd8a0810fcb56aab285e361d87dfe902d82395a3
Jesse Harris <zigford@gmail.com>
2016-06-04 18:56:43 +1000

    Updated scripts to support pulling and updating from git

--- scripts/scriptupdate.sh
+++ scripts/scriptupdate.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 if ifconfig wlan0 &>/dev/null; then
-    jessepi=10.1.1.250
+    jessepi=10.1.1.3
 else
     jessepi=zigford.ddns.net
 fi
 
-/home/pi/scripts/net-config.sh start
+/home/pi/lapsPi/scripts/net-config.sh start
 if [ $? == 0 ] ; then
-    rsync -avz -e ssh pi@${jessepi}:/home/pi/scripts /home/pi/
+    #rsync -avz -e ssh pi@${jessepi}:/home/pi/scripts /home/pi/
+    cd ~/lapsPi
+    git pull
 fi
-/home/pi/scripts/net-config.sh stop
-crontab /home/pi/scripts/crontab
-sudo cp -f /home/pi/scripts/superscript /etc/init.d/superscript
+/home/pi/lapsPi/scripts/net-config.sh stop
+crontab /home/pi/lapsPi/scripts/crontab
+sudo cp -f /home/pi/lapsPi/scripts/superscript /etc/init.d/superscript
 
