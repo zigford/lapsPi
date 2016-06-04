ac0e69d08b4fb7e28f7bac7b9b6ca0b3cf31a8c8
Jesse Harris <zigford@gmail.com>
2016-06-04 17:26:12 +1000

    updated paths to support cloneing into the source and runnning without moving aroudn

--- scripts/pictaker.sh
+++ scripts/pictaker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/pgrep -f timelapse.py || /usr/bin/nohup /usr/bin/python /home/pi/scripts/timelapse.py -W 1944 -H 1458 -n 1 >> /home/pi/timelapse.log &
+/usr/bin/pgrep -f timelapse.py || /usr/bin/nohup /usr/bin/python /home/pi/lapsPi/scripts/timelapse.py -W 1944 -H 1458 -n 1 >> /home/pi/timelapse.log &
