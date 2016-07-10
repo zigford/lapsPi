7bded0377d20299da41a28af76e4e7e22af29dca
Jesse Harris <zigford@gmail.com>
2016-07-10 15:39:05 +1000

    Now uploads at each picture due to no more power concerns

--- scripts/pictaker.sh
+++ scripts/pictaker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 /usr/bin/pgrep -f timelapse.py || /usr/bin/nohup /usr/bin/python /home/pi/lapsPi/scripts/timelapse.py -W 2592 -H 1944 -n 1 >> /home/pi/timelapse.log &
+/home/pi/lapsPi/scripts/uploader.sh >> /home/pi/uploader.log
