fd80c71e2d9faa3d54577e031592a5960eb5d42b
Jesse Harris <zigford@gmail.com>
2016-07-10 16:12:45 +1000

    added sleep between pictaker and uploader

--- scripts/pictaker.sh
+++ scripts/pictaker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 /usr/bin/pgrep -f timelapse.py || /usr/bin/nohup /usr/bin/python /home/pi/lapsPi/scripts/timelapse.py -W 2592 -H 1944 -n 1 >> /home/pi/timelapse.log &
+sleep 2m
 /home/pi/lapsPi/scripts/uploader.sh >> /home/pi/uploader.log
